5f53d977a90281ff8a9b0cea854331091d55c216
Zaheer <112798052+Zaheer-10@users.noreply.github.com>
2023-10-10 08:20:10 +0530

    Added QA with langchain intergration

---
+++ langchain_integration.py
@@ -0,0 +1,25 @@
+import chainlit as cl
+import openai
+import os
+from langchain import PromptTemplate, OpenAI, LLMChain
+
+api_key = os.environ.get("API_KEY")
+
+template = """Question: {question}
+
+Answer: Let's think step by step."""
+
+@cl.on_chat_start
+def main():
+    prompt = PromptTemplate(template=template, input_variables = ["question"])
+    llm_chain = LLMChain(prompt = prompt,llm=OpenAI(temperature=0,streaming=True),verbose=True)
+
+    cl.user_session.set("llm_chain",llm_chain)
+
+@cl.on_message
+async def main(message : str):
+    llm_chain = cl.user_session.get("llm_chain")
+
+    res = await llm_chain.acall(message, callbacks=[cl.AsyncLangchainCallbackHandler()])
+
+    await cl.Message(content=res["text"]).send()
